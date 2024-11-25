e849b5e733c692d43ed55a12da3633983e943627
0031432412016 <0031432412016@LTI3102>
2024-11-25 14:39:10 -0300

    Novas Colunas Adicionadas!

---
+++ ATVcadCliente/Migrations/20241125173709_AddColumsAllNew.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ATVcadCliente.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumsAllNew : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CPF",
+                table: "Cliente",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataNasc",
+                table: "Cliente",
+                type: "datetime2",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Endereco",
+                table: "Cliente",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EstCivil",
+                table: "Cliente",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Mae",
+                table: "Cliente",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Pai",
+                table: "Cliente",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RG",
+                table: "Cliente",
+                type: "nvarchar(11)",
+                maxLength: 11,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CPF",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "DataNasc",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "Endereco",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "EstCivil",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "Mae",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "Pai",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "RG",
+                table: "Cliente");
+        }
+    }
+}
--- ATVcadCliente/Models/Cliente.cs
+++ ATVcadCliente/Models/Cliente.cs
@@ -11,6 +11,21 @@ namespace ATVcadCliente.Models
         public string Nome { get; set; }
         [Required]
         [StringLength(100)]
+        public string CPF { get; set; }
+        [Required]
+        [StringLength(11)]
+        public string RG { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string Endereco { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string EstCivil { get; set; }
+        [Required]
+        [StringLength(100)]
+        public DateTime DataNasc { get; set; }
+        [Required]
         public string Pai { get; set; }
+        public string Mae { get; set; }
     }
 }
